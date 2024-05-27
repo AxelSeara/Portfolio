08617a2561f5315bbb6af87f09b4f00bb54dcb1c
AxSeGo <axel.seara.gomez@students.beaminstitute.org>
2024-05-27 15:32:10 +0200

    app working test & heroku push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { motion } from 'framer-motion';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App flex justify-center items-center h-screen bg-white">
+      <header className="text-center">
+        <motion.p
+          className="text-red-500 text-4xl font-bold"
+          animate={{ rotate: 360 }}
+          transition={{ repeat: Infinity, duration: 5, ease: "linear" }}
         >
-          Learn React
-        </a>
+          We are working on it
+        </motion.p>
       </header>
     </div>
   );
