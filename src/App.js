ac99a7a5db091dd5b14a09e86f51c71aa222d8f5
AxSeGo <axel.seara.gomez@students.beaminstitute.org>
2024-06-05 13:57:59 +0200

    distribution for phone devices on modal

--- src/App.js
+++ src/App.js
@@ -23,29 +23,16 @@ const App = () => {
   return (
     <MainLayout>
       <Navbar name="AXEL S" links={['About', 'CV', 'o_o']} />
-      <div className="relative flex-1 mt-16 h-full w-full">
-        <div className="grid grid-cols-2 gap-4 lg:grid-cols-4 lg:gap-8">
-          {folders.slice(0, 4).map((folder, index) => {
+      <div className="relative mt-16 h-full w-full">
+        <div className="grid grid-cols-3 gap-4 lg:grid-cols-4 lg:gap-8 mx-4">
+          {folders.map((folder, index) => {
             const FolderComponent = ['CV', 'DailyBloom', 'Weather'].includes(folder.name) ? FolderNoDoubleClick : Folder;
             return (
               <FolderComponent
                 key={folder.name}
                 initialOpen={false}
-                className="absolute"
-                style={{ top: `${100 + Math.floor(index / 2) * 120}px`, left: `${80 + (index % 2) * 140}px` }}
-                name={folder.name}
-                content={folder.content}
-              />
-            );
-          })}
-          {folders.slice(4).map((folder, index) => {
-            const FolderComponent = ['CV', 'DailyBloom', 'Weather'].includes(folder.name) ? FolderNoDoubleClick : Folder;
-            return (
-              <FolderComponent
-                key={folder.name}
-                initialOpen={false}
-                className="absolute"
-                style={{ top: `${100 + index * 120}px`, right: '80px' }}
+                className="relative"
+                style={{ marginTop: `${index >= 3 ? '20px' : '0'}` }}
                 name={folder.name}
                 content={folder.content}
               />
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,7 +8,7 @@ const Modal = ({ isOpen, onClose, title, children, zIndex }) => {
   return (
     <div className="fixed inset-0 pointer-events-none flex items-center justify-center" style={{ zIndex }}>
       <motion.div
-        className="relative bg-red-200 text-accent border-t-4 border-l-4 border-white w-1/2 shadow-no-blur pointer-events-auto"
+        className="relative bg-red-200 text-accent border-t-4 border-l-4 border-white w-full h-full md:w-1/2 md:h-auto shadow-no-blur pointer-events-auto"
         drag
         dragConstraints={{ top: -200, left: -200, right: 200, bottom: 200 }}
       >
@@ -21,7 +21,7 @@ const Modal = ({ isOpen, onClose, title, children, zIndex }) => {
             X
           </button>
         </div>
-        <div className="p-2">
+        <div className="p-2 overflow-y-auto">
           {children}
         </div>
       </motion.div>
