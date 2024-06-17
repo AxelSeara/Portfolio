4c18d5db35e63980d26b997061b11ed7e2317e08
AxSeGo <axel.seara.gomez@students.beaminstitute.org>
2024-06-17 14:51:50 +0200

    gb add

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import bgImage from './bgs/bg3.svg'; // Adjust the path if necessary
 
 const MainLayout = ({ children }) => {
   return (
-    <div className="h-screen flex flex-col bg-quaternary overflow-hidden">
+    <div
+      className="h-screen flex flex-col overflow-hidden bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       {children}
     </div>
   );
